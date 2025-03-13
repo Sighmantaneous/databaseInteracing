7e94a5d86e06111f2c41aded42fb3323ea82839b
Simon <g00286398@atu.ie>
2025-03-13 14:17:01 +0000

    Adding insert example

---
+++ src/main/java/InsertExample.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class InsertExample {
+
+    public static void main(String[] args) throws SQLException {
+
+        // Connect to the database
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/sampleproducts", "simon", "password");
+
+        try {
+
+            // Insert a new record into the "users" table using a prepared statement.
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO product (name, price) VALUES (?, ?)");
+            stmt.setString(1, "Shovel");
+            stmt.setString(2, "15");
+            stmt.executeUpdate();
+
+            System.out.println("Insert completed successfully.");
+        } catch (SQLException ex) {
+
+            System.out.println("Record insert failed.");
+            ex.printStackTrace();
+        }
+        // Close the connection
+        conn.close();
+    }
+
+
+
+
+}
