5a8590f8697cca1fbbb7dbe64f6f80770380f5d6
Simon <g00286398@atu.ie>
2025-03-13 14:41:08 +0000

    Adding Query example that returns id name and price of all items priced at 25

---
+++ src/main/java/QueryExample.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class QueryExample {
+
+    public static void main(String[] args) throws SQLException {
+
+        // Connect to the database
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/sampleproducts", "simon", "password");
+
+        String query = "SELECT * FROM product WHERE price = 25 " ;
+
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while ( rs.next() ) {
+                int productId = rs.getInt("id");
+                String nameProduct = rs.getString("name");
+                String priced = rs.getString("price");
+                System.out.println(productId+"\t"+nameProduct +"\t" +priced);
+            }
+
+        } catch (SQLException ex) {
+
+            System.out.println("Record insert failed.");
+            ex.printStackTrace();
+        }
+        // Close the connection
+        conn.close();
+    }
+
+    }
+
